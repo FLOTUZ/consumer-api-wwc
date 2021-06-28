8a3e70f034f9b5c07aed620ad7434d1395756abc
Emmanuel <flotuz10@gmail.com>
2021-06-28 13:23:49 -0500

    Se conecto la api a wooCommerce
    Se ingresaron los endpoints de
    - Consultar por id
    - Consulta masiva
    - Actualización de producto
    - Consulta de ordenes

--- index.js
+++ index.js
@@ -1,43 +1,93 @@
-const querystring = require("querystring");
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
 
 const app = express();
 //Puerto del sistema
 const port = process.env.PORT || 4000;
 
+//Configuracion de la API de WooCommerce
+const wc = new WooCommerceRestApi({
+  url: "https://core.noobhuman.ninja/",
+  consumerKey: "ck_e0a402b26b0cdcdfcfc6436512b2eaba73a0d1ef",
+  consumerSecret: "cs_de15415916045ddfd809c5f71d2b1dd11e8243e0",
+  version: "wc/v3",
+});
+
 //Para aceptar origenes cruzados
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send({ msg: "Hola mundo" });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//                      [ WooCommerce ]
+
+//--------------Consultar solo un producto------------------
+app.get("/products-wc/:id", async (req, res) => {
+  const { id } = req.params;
+  const url = `products/${id}`;
+  await wc
+    .get(url)
+    .then((response) => {
+      res.json({ producto: response.data });
+    })
+    .catch((error) => {
+      res.json(error.response.data);
+    });
 });
 
-app.get("/hola", function (req, res) {
-  const store_url = "https://core.noobhuman.ninja";
-  const endpoint = "/wc-auth/v1/authorize";
-  const params = {
-    app_name: "Core",
-    scope: "read_write",
-    user_id: 1,
-    return_url: "https://google.com",
-    callback_url: "https://api-wwc.herokuapp.com/callback-endpoint",
-  };
-  const query_string = querystring.stringify(params).replace(/%20/g, "+");
-
-  console.log(store_url + endpoint + "?" + query_string);
-  res.json({ url: `${store_url}${endpoint}?${query_string}` });
+//---------Consultar todos los productos----------
+app.get("/products-wc", async (req, res) => {
+  await wc
+    .get("products")
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      res.json(error.response.data);
+    });
 });
 
-app.get("/return-page", function (req, res) {
-  let data = decodeURI(req.query);
-  res.json({ msg: "Se ha logeuado con wordpress", data: data });
+/* -----------Actualizar producto ----------
+  aca se le manda el parametro a actualizar
+  por ejemplo:
+      Si se desea actualizar la descripcion
+      se manda la query con la key "description"
+      con su respectivo valor... algo como:
+          const data = {
+            description: "Nueva descripcion"
+          }
+
+*/
+app.put("/products-wc/:id", async (req, res) => {
+  const { id } = req.params;
+  const datosActualizados = req.query;
+
+  console.log(datosActualizados);
+
+  const url = `products/${id}`;
+
+  await wc
+    .put(url, datosActualizados)
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      res.json(error.response.data);
+    });
 });
 
-app.get("/callback-endpoint", function (req, res) {
-  let data = decodeURI(req.query);
-  console.log(data);
-  res.json({ msg: "Se ha logeuado con wordpress", data: data });
+app.get("/nueva-orden", async (req, res) => {
+  await wc
+    .get("orders")
+    .then((response) => {
+      res.json({ordenes: response.data});
+    })
+    .catch((error) => {
+      res.json(error.response.data);
+    });
 });
 
 app.listen(port, () => {
